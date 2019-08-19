794053f1c24be51c1db15e7cc833f97d1766f54d
Michael Ortega <michael@assetpanda.com>
2019-08-19 15:41:31 -0500

    Finish layout and routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+test "layout links" do
+  get root_path
+  assert_template 'static_pages/home'
+  assert_select "a[href=?]", root_path, count: 2
+  assert_select "a[href=?]", help_path
+  assert_select "a[href=?]", about_path
+  assert_select "a[href=?]", newpage_path
+  get newpage_path
+  assert_select "title", fll_title("NewPage")
+end
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title, help_path
+    assert_equal full_title("Help"), help_path
+  end
+
+end
